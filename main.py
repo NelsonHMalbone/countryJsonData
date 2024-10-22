51d6799e97d6a1901b6598d5216e438f5f857668
Nelson Malbone <nhmalbone@icloud.com>
2024-10-22 18:43:48 -0400

    10/22 initial commit
    imported requests
    
    added a input for user to enter country and then it will print out the capital of what user inputed

--- main.py
+++ main.py
@@ -7,11 +7,12 @@ content = response.json() #structure list
 
 # getting a input using a hard code word for now
 # later will get user input
-country = 'Iceland'
+country = input("Enter a Country: ")
 
 # acccss data
 for c in content:
     country_name = c['name']['common']
-    #if
-
+    if country_name == country:
+        capital = c['capital'][0]
+        print(capital)
 
